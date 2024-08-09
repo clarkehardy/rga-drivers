57b021b7dc0b24d7a575c7b7d915c389846693d5
Clarke Hardy <cahardy@stanford.edu>
2024-08-08 20:00:16 -0700

    testing user inputs

--- src/lxedrivers/tasks.py
+++ src/lxedrivers/tasks.py
@@ -6,4 +6,5 @@ class IsotopeAnalysis(AnalogScanTask):
     def __init__(self):
         print('Initializing IsotopeAnalysis class...')
         super().__init__()
+        self.output_dir = input('Enter the output directory: ')
         

ec4851671936e8601887c637afb7311d322b1403
Clarke Hardy <cahardy@stanford.edu>
2024-08-08 20:03:50 -0700

    testing user inputs

--- src/lxedrivers/tasks.py
+++ src/lxedrivers/tasks.py
@@ -3,8 +3,9 @@ from srsinst.rga import RGA100
 
 class IsotopeAnalysis(AnalogScanTask):
 
+    OutputDirectory = 'Save files to'
+
     def __init__(self):
         print('Initializing IsotopeAnalysis class...')
         super().__init__()
-        self.output_dir = input('Enter the output directory: ')
         
